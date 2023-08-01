fd302a0e62fbcad61483b72869ddf31429d04900
SeongHyeon Park <seonghye0n@kakao.com>
2023-08-01 14:36:57 +0900

    #23 conf: git workflow 생성 및 application.yml 적용

--- src/main/java/com/example/miniproject/MiniProjectApplication.java
+++ src/main/java/com/example/miniproject/MiniProjectApplication.java
@@ -2,8 +2,10 @@ package com.example.miniproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("file:${user.dir}/.env")
 public class MiniProjectApplication {
 
 	public static void main(String[] args) {
