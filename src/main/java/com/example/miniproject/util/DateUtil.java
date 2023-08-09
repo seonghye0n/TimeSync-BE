751905f75764cf451dc63e1ed4a4fb11e8eea9f7
SeongHyeon Park <seonghye0n@kakao.com>
2023-08-09 15:49:28 +0900

    #96 refactor: 연차/당직 등록, 수정 리팩토링

--- src/main/java/com/example/miniproject/util/DateUtil.java
+++ src/main/java/com/example/miniproject/util/DateUtil.java
@@ -1,5 +1,6 @@
 package com.example.miniproject.util;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -11,5 +12,11 @@ public class DateUtil {
 
 	public static String toDateFormat(LocalDateTime dateTime) {
 		return dateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+	}
+
+	public static int getDateDiff(LocalDateTime startDate, LocalDateTime endDate) {
+		int diff = (int)Duration.between(startDate, endDate).toDays() + 1;
+
+		return diff;
 	}
 }
