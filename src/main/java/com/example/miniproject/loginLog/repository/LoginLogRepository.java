e862ec43a4ec2d3b978137dbabfdac18cf24fca2
SYH <syh05247@naver.com>
2023-08-10 14:43:05 +0900

    #110 refactor: 사용하지 않는 코드 부분 삭제
    * loginLog 기록시 필요하지 않는 코드는 삭제

--- src/main/java/com/example/miniproject/loginLog/repository/LoginLogRepository.java
+++ src/main/java/com/example/miniproject/loginLog/repository/LoginLogRepository.java
@@ -4,7 +4,4 @@ import com.example.miniproject.loginLog.domain.LoginLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LoginLogRepository extends JpaRepository<LoginLog, Long> {
-
-    LoginLog findByMemberId(Long memberId);
-
 }
