bbb46141dfe856406b6154cfadd4d45106201e17
SYH <syh05247@naver.com>
2023-08-11 14:37:48 +0900

    #119 fix: cors, domain 수정
    * 프론트 쪽에 배포가 완료됨에 따라 cors 정책 및 ResponseCookie 의 도메인을 다시 한 번 수정하였다.

--- src/main/java/com/example/miniproject/member/controller/TokenApiController.java
+++ src/main/java/com/example/miniproject/member/controller/TokenApiController.java
@@ -34,7 +34,7 @@ public class TokenApiController {
                 .path("/")
                 .maxAge(60 * 60 * 24)
                 .sameSite("None")
-                .domain("https://hmteresting.netlify.app")
+                .domain("https://leeyongsu-calendar--hmteresting.netlify.app")
                 .build();
 
         return ResponseEntity.ok()
